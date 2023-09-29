6506f72bc20f0bfa7417cdc5646fd899bd239bd9
Marcos <mbinoti@gmail.com>
2023-09-29 19:31:00 -0300

    init

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,8 +12,6 @@ import 'package:spot/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // await tester.pumpWidget(const MyApp());
-
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
